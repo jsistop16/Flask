f0a0962adf7ca0c468bea7af73f1f5a7291866e8
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-09-12 20:27:17 +0900

    Q&A 게시판

---
+++ myproject/config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+#db접속주소
+#pybo.dp라는 db파일을 루트디렉토리에 저장한다는 뜻
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "dev"
